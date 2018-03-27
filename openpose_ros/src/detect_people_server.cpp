a5a1c409a32e65669d7841be2375eebebd8f7108
Kai Konen <kkonen@techfak.uni-bielefeld.de>
2018-03-27 16:53:34 +0200

    one more fix

--- openpose_ros/src/detect_people_server.cpp
+++ openpose_ros/src/detect_people_server.cpp
@@ -343,6 +343,7 @@ openpose_ros_msgs::PersonAttributes getAttributes(std::string uuid) {
                     sensor_msgs::ImagePtr inputImage_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", crop).toImageMsg();
                     srv.request.objects.push_back(*inputImage_msg);
                 } catch (cv::Exception e) {
+
                         std::cout << "Exception in gender and age! ROI could be wrong!" << e.what();
                 }
 
@@ -364,7 +365,7 @@ openpose_ros_msgs::PersonAttributes getAttributes(std::string uuid) {
             face_client_ptr.get()->call(srv);
             ROS_INFO("gasize: %u",(int)srv.response.gender_and_age_response.gender_and_age_list.size());
             ROS_INFO("personsize: %u",(int)person_list.size());
-            for (size_t i = 0; i < person_list.size(); ++i) {
+            for (size_t i = 0; i < srv.response.gender_and_age_response.gender_and_age_list.size(); ++i) {
                     person_list.at(i).gender_hyp = srv.response.gender_and_age_response.gender_and_age_list.at(i).gender_probability;
                     person_list.at(i).age_hyp = srv.response.gender_and_age_response.gender_and_age_list.at(i).age_probability;
             }
